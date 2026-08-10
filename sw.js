6a802d82f3c18005598f6e56864bfde90056f6b8
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-08-10 11:53:51 -0300

    atualiza cache do fluxo administrativo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drogaria-rocha-v6';
+const CACHE = 'drogaria-rocha-v7';
 const BASE = '/drogaria-rocha-app/';
 const APP_SHELL = [BASE, `${BASE}index.html`, `${BASE}manifest.webmanifest`, `${BASE}icons/icon.svg`];
 
