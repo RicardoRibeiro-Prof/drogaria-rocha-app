c5fb7144ef28231f2ac6badcc94a6c0471b1acd7
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-08-11 19:15:24 -0300

    Atualiza sw.js para o novo layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drogaria-rocha-v26';
+const CACHE = 'drogaria-rocha-v27';
 const BASE = '/drogaria-rocha-app/';
 const APP_SHELL = [BASE, `${BASE}index.html`, `${BASE}manifest.webmanifest`, `${BASE}icons/icon.svg`];
 
