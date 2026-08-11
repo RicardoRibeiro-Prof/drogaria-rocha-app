2b8940e1cc3cf8fab698762b3a4aae6b4e209766
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-08-11 18:44:51 -0300

    renova cache para secoes completas v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drogaria-rocha-v24';
+const CACHE = 'drogaria-rocha-v25';
 const BASE = '/drogaria-rocha-app/';
 const APP_SHELL = [BASE, `${BASE}index.html`, `${BASE}manifest.webmanifest`, `${BASE}icons/icon.svg`];
 
