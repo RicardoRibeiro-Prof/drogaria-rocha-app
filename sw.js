d811e0a0a273ad0ec7e05a47d98eaa232e5bfb11
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-08-11 19:06:58 -0300

    Renova cache da nova interface

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'drogaria-rocha-v25';
+const CACHE = 'drogaria-rocha-v26';
 const BASE = '/drogaria-rocha-app/';
 const APP_SHELL = [BASE, `${BASE}index.html`, `${BASE}manifest.webmanifest`, `${BASE}icons/icon.svg`];
 
