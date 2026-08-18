0e14f048a180d527349c6c5471ecdbccc4f71e17
RicardoRibeiro-Prof <ricardorpaes@hotmail.com>
2026-08-18 20:29:15 -0300

    Ajusta área segura inferior no Android

--- App.tsx
+++ App.tsx
@@ -1,1 +1,13 @@
-export { default } from './src/AppV2';
+import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import AppV2 from './src/AppV2';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
+        <AppV2 />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
